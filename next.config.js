87dcc0479d2a452e750703563a9e39ff1bc921d9
imash96 <arshad.ans7006@gmail.com>
2023-01-09 22:54:21 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 const { withStoreConfig } = require("./store-config")
 const store = require("./store.config.json")
 
+async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ]
+  },
+
 module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
