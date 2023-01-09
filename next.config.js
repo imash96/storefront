26e6923684878726f5a19f24b6720b113a22c3f9
imash96 <arshad.ans7006@gmail.com>
2023-01-09 23:34:32 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
-    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost", 'backend.leatherlifestyle.store', 'https://backend.leatherlifestyle.store'],
+    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost"],
   },
 })
 
