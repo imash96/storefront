30ec740436194edefaf6d239f3ad5af478207d43
imash96 <arshad.ans7006@gmail.com>
2023-01-09 23:22:53 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,8 +5,25 @@ module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
-    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost"],
+    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost", 'backend.leatherlifestyle.store', 'https://backend.leatherlifestyle.store'],
   },
 })
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
+};
 
 console.log("next.config.js", JSON.stringify(module.exports, null, 2))
