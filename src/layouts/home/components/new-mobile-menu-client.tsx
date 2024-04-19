f9ea715a7ce8ebf100bd63ece75c9b469ee7915d
Arshad Ansari <arshad.ans7006@gmai.com>
2024-04-19 21:50:00 +0530

    initial commit

--- src/layouts/home/components/new-mobile-menu-client.tsx
+++ src/layouts/home/components/new-mobile-menu-client.tsx
@@ -16,7 +16,7 @@ export default function MobileMenuClient({ children, regions }: { regions: Regio
             <div className={`absolute inset-0 z-30 bg-curtain transition-opacity duration-500 ease-in-out delay-[0.35s] ${menuState ? "opacity-25" : "opacity-0"}`} onClick={mobileMenuClose} />
             <div className="absolute inset-0 z-40 flex w-11/12 sm:w-5/6 max-w-md border-r border-grey-11">
                 <div className="relative flex w-full flex-col shadow-xl bg-scroll bg-naviBg text-grey-33 bg-grey-20">
-                    <div className="flex justify-between px-4 py-3 sticky top-0 border-b border-grey-16 z-10 bg-grey-18">
+                    <div className="flex justify-between px-4 py-3 sticky top-0 border-b border-grey-17 z-10 bg-grey-18">
                         <button type="button" className="-m-2 items-center rounded-md p-2" onClick={mobileMenuClose}>
                             <span className="sr-only">Close menu</span>
                             <XMarkIcon className="h-6 w-6 fill-none" aria-hidden="true" />
@@ -26,7 +26,7 @@ export default function MobileMenuClient({ children, regions }: { regions: Regio
                         </div>
                     </div>
                     {children}
-                    <div className="flex justify-between px-4 py-3 sticky bottom-0 border-t border-grey-16 z-10 bg-grey-18">
+                    <div className="flex justify-between px-4 py-3 sticky bottom-0 border-t border-grey-17 z-10 bg-grey-18">
                         {/* Account */}
                         <LocalizedClientLink href="/account" className="p-2 hover:text-grey-83 active:text-grey-85 ml-4 md:hidden" onClick={mobileMenuClose}>
                             <span className="sr-only">Account</span>
--- src/layouts/home/components/new-mobile-menu-server.tsx
+++ src/layouts/home/components/new-mobile-menu-server.tsx
@@ -1,11 +1,28 @@
 import LocalizedClientLink from "@modules/common/localized-client-link"
 import MobileMenuAction from "./mobile-menu-action"
 import MobileMenuContact from "./mobile-menu-contact"
+import MobileMenuTab from "./new-mobile-menu-tab"
+import { medusaClient } from "@libs/config"
+import { ProductCategory } from "@medusajs/medusa"
+import { getRandomCategory } from "@libs/utils/get-random"
 
-
-export default function MobileMenuServer() {
+export default async function MobileMenuServer() {
+    const { product_categories } = await medusaClient.productCategories.list({ parent_category_id: "null", include_descendants_tree: true }, { next: { tags: ["categories"] } })
+    const main_category: ProductCategory[] = []
+    main_category.push(product_categories[0])
+    main_category.push(product_categories[1])
+    const new_category: ProductCategory[] = []
+    new_category[0] = {
+        ...main_category[0],
+        category_children: getRandomCategory(main_category[0].category_children, 12)
+    } as ProductCategory
+    new_category[1] = {
+        ...main_category[1],
+        category_children: getRandomCategory(main_category[1].category_children, 12)
+    } as ProductCategory
     return (
         <div className="overflow-y-auto bg-grey-21/80 mb-auto px-4 h-full">
+            <MobileMenuTab categories={new_category} />
             <MobileMenuAction className="border-t border-grey-17 py-2 text-sm font-medium">
                 {Object.entries(pages).map(([name, href]) => (
                     <div key={name} className="py-px text-left text-grey-81">
